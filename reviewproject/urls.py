037ca3d913e16f4eb1d1a520f009d5574e20f811
unknown <ohao218@likelion.org>
2019-07-29 16:15:30 +0900

    commit for deploy4

--- reviewproject/urls.py
+++ reviewproject/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 import diaryapp.views
 import userapp.views
 
@@ -30,4 +28,4 @@ urlpatterns = [
     path('diaryapp/postupdate/<int:post_id>', diaryapp.views.postupdate, name='postupdate'),
     path('diaryapp/postdelete/<int:post_id>', diaryapp.views.postdelete, name='postdelete'),
     path('userapp/', include('userapp.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
